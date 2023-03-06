639681e7fc88b710a9446fc5585093be5e4583b4
Yasmine Robles <yasmine@roblesdesigns.com>
2023-03-06 06:58:56 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Targeting area that will populate the profile.
+const overview = document.querySelector(".overview");
+//Selecting the username
+const username = "yasminerobles";
+//selecting the UL to display repos
+const repoList = document.querySelector(".repo-list");
+
+const gitInfo = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+  const data = await userInfo.json();
+    displayUserInfo(data);
+};
+
+gitInfo();
+
+const displayUserInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>
+  `;
+  overview.append(div);
+  gitRepos();
+};
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      repoList.append(repoItem);
+    }
+  };
